c0fe961a6dcbd7f9435ab228c116a99f70ecbb55
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-09-04 11:49:17 -0300

    add object newExpense

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
+    // Obtém o valor atual do input e remove todos os caracteres não numéricos
+    let value = amount.value.replace(/\D/g, '');
+
+    // Transforma o valor em centavos
+    value = Number(value) / 100
     
+    // Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value);
+
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+    // Retorna o valor formatado
+    return value
+}
+
+// Captura o evento de submit para obter os valores
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página
+    event.preventDefault();
+
+    // Cria um objeto com os detalhes na nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        create_at: new Date(),
+    }
 }
